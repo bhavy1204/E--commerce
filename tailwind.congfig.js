f00af94df0e8ccc310e6b8612b587e3a92bb1458
bhavy1204 <bhavysarwa51@gmail.com>
2025-09-17 10:14:59 +0530

    initial project setup

---
+++ tailwind.congfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#FF6347',
+                'secondary': {
+                    DEFAULT: '#6A5ACD', // Default shade for secondary
+                    light: '#8A7ACD', // Light shade
+                    dark: '#4A3ACD', // Dark shade
+                },
+            },
+        },
+    },
+};
